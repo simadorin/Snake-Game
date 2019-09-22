63d20025436fd6b524c61945cc0383f69ce4588a
simadorin <55402362+simadorin@users.noreply.github.com>
2019-09-22 16:07:47 +0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,6 @@
+import random
+
+def roll(dices):
+    dices = (1,2,3,4,5,6)
+    return random.dices
+print(roll(dices))
---
+++ Ex.py
@@ -0,0 +1,8 @@
+def add_numbers(*args):
+    total = 0
+    for a in args:
+        total += a
+    print(total)
+
+add_numbers(3)
+add_numbers(2,3,4,5,6,7,4)
